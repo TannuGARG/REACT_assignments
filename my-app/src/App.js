3528fb4ba93960fd1f0061e847a44eec29407514
Tamanna garg <tgarg661@gmail.com>
2021-12-14 18:23:23 +0530

    done changes

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={"https://www.w3schools.com/howto/img_avatar.png"} className="profile" alt="profileimg" />
+        <Profile />
+      
+
+        
+    </div>
+  );
+}
+
+export default App;
