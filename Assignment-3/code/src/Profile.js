6fc2f95040106d0195773017dffc1c0dc1a42984
Tamanna garg <tgarg661@gmail.com>
2021-12-17 20:32:40 +0530

    testcases passed

--- Assignment-3/code/src/Profile.js
+++ Assignment-3/code/src/Profile.js
@@ -6,8 +6,9 @@ export default class Profile extends React.Component{
         return (
             <div className="container">
                 {/* first box */}
-                <div className="Box1">
-            
+                <div className="customer-info">
+
+                    
                     <div className="box11"><strong>{this.props.employee.Name}</strong></div>
                     <div className="box12">{this.props.employee.EmployeeID}</div>
                     <button className="box13">Print</button>
@@ -17,12 +18,12 @@ export default class Profile extends React.Component{
                 
                 {/* second box */}
                 <div className="Box2">
-                    <div className="Box2-1"><strong>Appointment:</strong>{this.props.employee.Appointment}</div>
+                    <div className="Box2-1"><strong>Appointment On</strong>{this.props.employee.Appointment}</div>
                     <div className="Box2-2"><strong>Email:</strong>{this.props.employee.Email}</div>
-                    <div className="Box2-3"><strong>Contact:</strong>{this.props.employee.Phone}</div>
+                    <div className="Box2-3"><strong>Phone:</strong>{this.props.employee.Phone}</div>
                 </div>
                 {/* third box */}
-                <div className="Box3">
+                <div className="order-info">
                     <div className="Box3-1">
                         <div className="Box3-1-1"><strong>Status</strong></div>
                         <span className="dot"></span>{this.props.order.Status}
@@ -42,9 +43,9 @@ export default class Profile extends React.Component{
 
                 </div>
                 {/* forth box */}
-                <div className="Box4">
+                <div className="product-list">
                     <button type="checkbox" className="checkbox"> </button>
-                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Profe-photo" className="Photo"></img>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Profile" className="dev"/>
                     <div>
                         <div className="Box4-1"><strong>Boltaart Bosbessen</strong></div>
                         <div>Overheerlijke Boltaart met Bosbessen de ud keuken ven de askjcjvcd</div>
